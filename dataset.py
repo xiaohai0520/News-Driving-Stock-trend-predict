use bert instead of glove but there is a bug of gradient disappear

dataset.py
@@ -1,17 +1,20 @@
 import re
-import gensim
+# import gensim
 import pandas as pd
 import pickle
 import numpy as np
 import nltk
-from gensim.scripts.glove2word2vec import glove2word2vec
-from gensim.models.keyedvectors import KeyedVectors
+# from gensim.scripts.glove2word2vec import glove2word2vec
+# from gensim.models.keyedvectors import KeyedVectors
 from nltk.corpus import stopwords
+from bert_serving.client import BertClient
 
+DRIVING = "D:\Projects\\stock_predict\\stock_data\\Predicting-the-Dow-Jones-with-Headlines-master\\News.csv"
+TARGET = "D:\Projects\\stock_predict\\stock_data\\Predicting-the-Dow-Jones-with-Headlines-master\\DowJones.csv"
 
 # global file path
-file = "D:\\Projects\\stock_predict\\glove.6B\\glove.6B.50d.txt"
-glove_vector_name = "gensim_glove_vectors.txt"
+# file = "D:\\Projects\\stock_predict\\glove.6B\\glove.6B.50d.txt"
+# glove_vector_name = "gensim_glove_vectors.txt"
 embedding_dim = 50
 
 # A list of contractions from http://stackoverflow.com/questions/19790188/expanding-english-language-contractions-in-python
@@ -96,21 +99,28 @@ class DataSet():
 
     def __init__(self,newsfile,pricesfile,time_step,split_ratio=0.6):
 
+        #the news file path
         self.newsfile = newsfile
 
+        #the price file path
         self.pricesfile = pricesfile
 
+        #create a BERT Client object to do the sentence embedding
+        self.bc = BertClient(check_length=False)
 
         #transfer the txt into w2v file
-        self.transfer_model(file,glove_vector_name)
+        # self.transfer_model(file,glove_vector_name)
 
         #get the word2vec model
-        self.model = self.load_glove_model(glove_vector_name)
+        # self.model = self.load_glove_model(glove_vector_name)
 
+
+        #read from the csv
         prices_trend, headlines = self.get_all_data()
 
         self.news, self.y, self.y_seq = self.create_dataset(prices_trend, headlines, time_step)
 
+        print(self.news.shape)
 
         self.train_size = int(split_ratio * (len(self.y) - time_step - 1))
 
@@ -135,33 +145,41 @@ class DataSet():
         # return self.news[self.train_size:], self.y[self.train_size:], self.y_seq[self.train_size:]
 
 
-    def transfer_model(self,input, output):
-        """
-        transfer txt into model with glove lib
-        :param file: file path of the glove txt file
-        :return: None
-        """
-        glove2word2vec(glove_input_file=input, word2vec_output_file=output)
+    # def transfer_model(self,input, output):
+    #     """
+    #     transfer txt into model with glove lib
+    #     :param file: file path of the glove txt file
+    #     :return: None
+    #     """
+    #     glove2word2vec(glove_input_file=input, word2vec_output_file=output)
 
 
-    def load_glove_model(self,input):
-        """
-        get the glove model
-        :param filename: the file name
-        :return: glove model
-        """
-        return KeyedVectors.load_word2vec_format(input, binary=False)
+    # def load_glove_model(self,input):
+    #     """
+    #     get the glove model
+    #     :param filename: the file name
+    #     :return: glove model
+    #     """
+    #     return KeyedVectors.load_word2vec_format(input, binary=False)
 
     def read_news(self):
         """
+        read the data from the csv
         :return: both of the news and stock price
         """
         dj = pd.read_csv(self.pricesfile)
         news = pd.read_csv(self.newsfile)
         return dj, news
 
+
     def clean_text(self,text, remove_stopwords=True):
-        '''Remove unwanted characters and format the text to create fewer nulls word embeddings'''
+        """
+        Remove unwanted characters and format the text to create fewer nulls word embeddings
+        :param text: text
+        :param remove_stopwords: boolean to remove the stopword
+        :return: clleaned news
+        """
+
 
         # Convert words to lower case
         text = text.lower()
@@ -205,7 +223,10 @@ class DataSet():
         return text
 
     def get_data_from_csv(self):
-
+        """
+        read data from csv and return the array
+        :return: stock trend and news array
+        """
         dj, news = self.read_news()
 
         # search the null length
@@ -217,15 +238,15 @@ class DataSet():
         # print(news.shape)
 
         # Compare the number of unique dates. We want matching values.
-        print(len(set(dj.Date)))
-        print(len(set(news.Date)))
+        # print(len(set(dj.Date)))
+        # print(len(set(news.Date)))
 
         # Remove the extra dates that are in news
         news = news[news.Date.isin(dj.Date)]
 
         # make sure the equal
-        print(len(set(dj.Date)))
-        print(len(set(news.Date)))
+        # print(len(set(dj.Date)))
+        # print(len(set(news.Date)))
 
         # use the date as index to get the different each day
         dj = dj.set_index('Date').diff(periods=1)
@@ -260,6 +281,11 @@ class DataSet():
         return prices_trend, headlines
 
     def clean_new(self,headlines):
+        """
+        clean the whole news data
+        :param headlines: the news data set
+        :return: cleaned news data set
+        """
         clean_headlines = []
 
         for daily_headlines in headlines:
@@ -269,91 +295,144 @@ class DataSet():
             clean_headlines.append(clean_daily_headlines)
         return clean_headlines
 
-    def dailynews_to_vector(self,news_corpos):
-        """
-        :return: daily news numpy array
-        """
 
-        #     news_vectors = np.empty(len(news_corpos))
-        news_vectors = []
-        for news in news_corpos:
-            ls = news.split()
-            length = len(ls)
-            single_news_vec = []
-            for word in ls:
-                #             print(word)
-                try:
-                    single_news_vec.append(np.asarray(self.model[word]))
-                except KeyError:
-                    #                 print('No word')
-                    single_news_vec.append(np.random.uniform(-1.0, 1.0, embedding_dim))
-            #size 22 * 50
-            single_news_vec = np.array(single_news_vec)
-            # size n * 50
+    # def dailynews_to_vector(self,news_corpos):
+    #     """
+    #
+    #     :return: daily news numpy array
+    #     """
+    #
+    #     #     news_vectors = np.empty(len(news_corpos))
+    #     news_vectors = []
+    #     for news in news_corpos:
+    #         ls = news.split()
+    #         length = len(ls)
+    #         single_news_vec = []
+    #         for word in ls:
+    #             #             print(word)
+    #             try:
+    #                 single_news_vec.append(np.asarray(self.model[word]))
+    #             except KeyError:
+    #                 #                 print('No word')
+    #                 single_news_vec.append(np.random.uniform(-1.0, 1.0, embedding_dim))
+    #         #size 22 * 50
+    #         single_news_vec = np.array(single_news_vec)
+    #         # size n * 50
+    #
+    #         # print('single news vector:',single_news_vec.shape)
+    #
+    #         #
+    #         news_vectors.append(single_news_vec.mean(0))
+    #     # 22 * 50
+    #     news_vectors = np.array(news_vectors)
+    #     # print('daily total news vecotr:',news_vectors.shape)
+    #     return news_vectors
 
-            # print('single news vector:',single_news_vec.shape)
-
-            #
-            news_vectors.append(single_news_vec.mean(0))
-        # 22 * 50
-        news_vectors = np.array(news_vectors)
-        # print('daily total news vecotr:',news_vectors.shape)
-        return news_vectors
 
 
+    # def pooling(self,dailynews):
+    #     news_max = dailynews.max(0)
+    #     news_min = dailynews.min(0)
+    #     news_mean = dailynews.mean(0)
+    #
+    #     dailynews = np.concatenate((news_max,news_min,news_mean),axis=None)
+    #     return dailynews
 
-    def pooling(self,dailynews):
-        news_max = dailynews.max(0)
-        news_min = dailynews.min(0)
-        news_mean = dailynews.mean(0)
+    # def news_embedding(self,headlines):
+    #     headlines_vector = []
+    #     for dailynews in headlines:
+    #         day_news = self.dailynews_to_vector(dailynews)
+    #
+    #         headlines_vector.append(day_news)
+    #
+    #     return np.array(headlines_vector)
 
-        dailynews = np.concatenate((news_max,news_min,news_mean),axis=None)
-        return dailynews
 
     def news_embedding(self,headlines):
-        headlines_vector = []
-        for dailynews in headlines:
-            day_news = self.dailynews_to_vector(dailynews)
+        """
+        do the news embedding op
+        :param headlines: the whole news data set
+        :return: the data set of embedding news
+        """
+        res = []
+        for headline in headlines:
+            # print(headline)
 
-            headlines_vector.append(day_news)
+            hd_embedding_array = self.bc.encode(headline)
 
-        return np.array(headlines_vector)
+            # print(hd_embedding_array)
+            res.append(hd_embedding_array)
+        return np.array(res)
+
 
     def create_dataset(self,prices_trend, headlines, time_step):
+        """
+
+        :param prices_trend: stock list
+        :param headlines: news list
+        :param time_step: time step 10
+        :return: three np array
+        """
         x, y, y_seq = [], [], []
         for i in range(len(prices_trend) - time_step - 1):
             last = i + time_step
             x.append(headlines[i:last])
+
+            #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             y.append(1 if prices_trend[last] > 0 else 0)
             y_seq.append(prices_trend[i:last])
+
         return np.array(x), np.array(y), np.array(y_seq)
 
     def get_all_data(self):
-        # transfer_model(file,glove_vector_name)
+        """
 
+        :return: the stock predict(list) and news np array
+        """
+
+        #get the original data of trend and headline
         prices_trend, headlines = self.get_data_from_csv()
 
+        print(prices_trend)
+        print(type(prices_trend))
+
+        #do the word cleaning
+
+        #stylle:[[news,news,news],
+        #        [news,news,news]
+        #        [news,news,news]]
         headlines = self.clean_new(headlines)
 
-        headlines = self.news_embedding(headlines)
+        for headline in headlines:
+
+            print(headline)
+
+
+        #do the news embedding
+
+        headlines = self.news_embedding(headlines[:20])
+
         # print(headlines[:5])
 
         #
         min_length = min(len(i) for i in headlines)
         max_length = max(len(i) for i in headlines)
         cur_headlines = []
+
+        #make full to 25
         for headline in headlines:
             if len(headline) == max_length:
                 cur_headlines.append(headline)
             else:
                 cur_headlines.append(np.concatenate((headline,np.zeros((max_length - len(headline),embedding_dim)))))
-        # headlines = [i for i in headlines if len(i) == max_length else i + ]
-        # headlines = [i[:min_length] for i in headlines]
-        # print(headlines[:10])
+
+
+        # prices type is list
         return prices_trend, np.array(cur_headlines)
 
 
 
-
+#
+# ds = DataSet(DRIVING, TARGET,10)
 
 

model.py
@@ -19,64 +19,102 @@ class Encoder(nn.Module):
         self.T = T
 
         self.news_linear = nn.Linear(in_features=embedding_dim, out_features=1)
+        self.attn1 = nn.Linear(in_features=2 * hidden_size, out_features=self.T)
+        self.attn2 = nn.Linear(in_features=self.T, out_features=self.T)
+        self.tanh = nn.Tanh()
+        self.attn3 = nn.Linear(in_features=self.T, out_features=1)
         self.lstm_layer = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=1)
-        self.attn_linear = nn.Linear(in_features=2 * hidden_size + T, out_features=1)
 
     def forward(self, input_data):
+        #batch size :128
+        batch_size = input_data.size(0)
         # print(input_data.shape)
+
+        #linear change: 128 * 10 * 25 * 768 -> 128 * 10 * 25 * 1
         input_data = self.news_linear(input_data)
         # print(input_data.shape)
+
+        #squeeze  128 * 10 * 25 * 1 -> 128 * 10 * 25
+        # input_data: (batch_size, T , input_size)
         input_data = input_data.squeeze(3)
         # print(input_data.shape)
-        # input_data: (batch_size, T , input_size)
-        input_weighted = Variable(torch.zeros(input_data.size(0), self.T, self.input_size))
-        input_encoded = Variable(torch.zeros(input_data.size(0), self.T, self.hidden_size))
+
+        # input_weighted: 128 * 10 * 25   input_encoded : 128 * 25 * 64
+        # input_weighted(batch_size, T, input_size)     input_encoded(batch_size, T, hidden_size)
+        input_weighted = Variable(torch.zeros(batch_size, self.T, self.input_size))
+        input_encoded = self.init_variable(batch_size,self.T,self.hidden_size)
+
         # hidden, cell: initial states with dimension hidden_size
-        hidden = self.init_hidden(input_data, self.hidden_size)  # 1 * batch_size * hidden_size
-        cell = self.init_hidden(input_data, self.hidden_size)
+        # hidden: 1 * 128 * 64   cell: 1 * 128 * 64
+        # hidden = cell : 1 * batch_size * hidden_size
+        hidden = self.init_variable(1, batch_size,self.hidden_size)
+        cell = self.init_variable(1, batch_size, self.hidden_size)
+
 
         for t in range(self.T):
-            # Eqn. 8: concatenate the hidden states with each predictor
-            hidden_change = hidden.repeat(self.input_size, 1, 1).permute(1, 0, 2)
-            # print(hidden.size())
-            cell_change = cell.repeat(self.input_size, 1, 1).permute(1, 0, 2)
-            # print(cell.size())
-            input_data_change = input_data.permute(0, 2, 1)
-            # print(input_data.size())
-            x= torch.cat((hidden_change,cell_change,input_data_change),dim=2)
-            # x = torch.cat((hidden.repeat(self.input_size, 1, 1).permute(1, 0, 2),
-            #                cell.repeat(self.input_size, 1, 1).permute(1, 0, 2),
-            #                input_data.permute(0, 2, 1)), dim=2)  # batch_size * input_size * (2*hidden_size + T )
-            # Eqn. 8: Get attention weights
-            # print(x.size())
-            x = self.attn_linear(x.view(-1, self.hidden_size * 2 + self.T))  # (batch_size * input_size) * 1
-            # print(x.size())
+
+            # Eqn. 8: concatenate the hidden states and the cell states
+
+            # 128 * 10 * 64 + 128 * 10 * 64 = 128 * 10 * 128
+            # batch_size * T * (2 * hidden_size)
+            x = torch.cat((self.embedding_hidden(hidden), self.embedding_hidden(cell)), dim=2)
+
+            # batch_size * input_size * (2 * hidden_size) -> batch_size * input_size * T
+            # 128 * 25 * 128 -> (z1) 128 * 25 * 10
+            z1 = self.attn1(x)
+            # print('z1:',z1.shape)
+
+            # batch_size * input_size * T  ->  batch_size * input_size * T
+            #  128 * 25 * 10  -> 128 * 25 * 10
+            z2 = self.attn2(input_data.permute(0, 2, 1))
+            # print('z2:', z2.shape)
+
+            # batch_size * input_size * T
+            #x: 128 * 25 * 10
+            x = z1 + z2
+
+            # batch_size * input_size * T  -> batch_size * input_size * 1
+            # 128 * 25 * 10 -> 128 * 25 * 1
+            z3 = self.attn3(self.tanh(x))
+
             # Eqn. 9: Softmax the attention weights
-            attn_weights = F.softmax(x.view(-1, self.input_size), dim=1)  # (batch_size, input_size)
-            # print(attn_weights.size())
-            # Eqn. 10: LSTM
-            # print('1',input_data[:,t,:].size())
-            weighted_input = torch.mul(attn_weights, input_data[:, t, :])  # (batch_size, input_size)
-            # print(weighted_input.size())
+            # (batch_size, input_size)  128 * 25
+            if batch_size > 1:
+                attn_weights = F.softmax(z3.view(batch_size, self.input_size), dim=1)
+            else:
+                attn_weights = self.init_variable(batch_size, self.input_size) + 1
+
+
+            # attn_weights = F.softmax(x.view(-1, self.input_size), dim=1)
+
+            # Eqn. 10: update driving series
+            # (batch_size, input_size)   128 *25
+            weighted_input = torch.mul(attn_weights, input_data[:, t, :])
+
             # Fix the warning about non-contiguous memory
             # see https://discuss.pytorch.org/t/dataparallel-issue-with-flatten-parameter/8282
-            self.lstm_layer.flatten_parameters()
+            # self.lstm_layer.flatten_parameters()
+
+            # Eqn. 11: LSTM
+            # weighted_input.unsqueeze(0) : 1 * 128 * 25
             _, lstm_states = self.lstm_layer(weighted_input.unsqueeze(0), (hidden, cell))
             hidden = lstm_states[0]
             cell = lstm_states[1]
             # Save output
 
-            input_weighted[:, t, :] = weighted_input
+            # input_weighted[:, t, :] = weighted_input
             input_encoded[:, t, :] = hidden
 
-        return input_weighted, input_encoded
+        return input_encoded
 
-    def init_hidden(self,x, hidden_size):
+    def embedding_hidden(self, x):
+        return x.repeat(self.input_size, 1, 1).permute(1, 0, 2)
+
+    def init_variable(self, *args):
+        zero_tensor = torch.zeros(args)
         if torch.cuda.is_available():
-            return Variable(torch.zeros(1, x.size(0), hidden_size)).cuda()
-        else:
-            return Variable(torch.zeros(1, x.size(0), hidden_size))
-
+            zero_tensor = zero_tensor.cuda()
+        return Variable(zero_tensor)
 
 
 class Decoder(nn.Module):
@@ -84,69 +122,107 @@ class Decoder(nn.Module):
     def __init__(self, encoder_hidden_size, decoder_hidden_size, T, out_feats=2):
         super(Decoder, self).__init__()
 
-        self.T = T
+
         self.encoder_hidden_size = encoder_hidden_size
         self.decoder_hidden_size = decoder_hidden_size
+        self.T = T
 
-        self.attn_layer = nn.Sequential(nn.Linear(2 * decoder_hidden_size + encoder_hidden_size,
-                                                  encoder_hidden_size),
-                                        nn.Tanh(),
-                                        nn.Linear(encoder_hidden_size, 1))
-        self.lstm_layer = nn.LSTM(input_size=out_feats, hidden_size=decoder_hidden_size)
-        self.fc = nn.Linear(encoder_hidden_size + T, out_feats)
-        self.fc_final = nn.Linear(decoder_hidden_size + encoder_hidden_size, out_feats)
+        self.attn1 = nn.Linear(in_features=2*decoder_hidden_size, out_features=encoder_hidden_size)
+        self.attn2 = nn.Linear(in_features=encoder_hidden_size,out_features=encoder_hidden_size)
+        self.tanh = nn.Tanh()
+        self.attn3 = nn.Linear(in_features=encoder_hidden_size, out_features=1)
 
-        self.fc.weight.data.normal_()
+        self.lstm_layer = nn.LSTM(input_size=1, hidden_size=decoder_hidden_size)
+        self.tilde = nn.Linear(in_features=encoder_hidden_size + 1, out_features=1)
+        self.fc1 = nn.Linear(in_features=decoder_hidden_size + encoder_hidden_size,out_features=decoder_hidden_size)
+        self.fc2 = nn.Linear(in_features=decoder_hidden_size,out_features=out_feats)
+        # self.fc.weight.data.normal_()
 
-    def forward(self, input_encoded, y_history):
-        # input_encoded: (batch_size, T - 1, encoder_hidden_size)
-        # y_history: (batch_size, (T-1))
-        # Initialize hidden and cell, (1, batch_size, decoder_hidden_size)
+    def forward(self, input_encoded, y_seq):
+        #batch_size : 128
+        batch_size = input_encoded.size(0)
 
-        # ？？？？？？why not on gpu
-        if torch.cuda.is_available():
-            input_encoded = input_encoded.cuda()
-        hidden = self.init_hidden(input_encoded, self.decoder_hidden_size)
-        cell = self.init_hidden(input_encoded, self.decoder_hidden_size)
-        context = Variable(torch.zeros(input_encoded.size(0), self.encoder_hidden_size))
+        # hidden : 1 * batch_size * decode hidden size    1 * 128 * 64
+        hidden = self.init_variable(1,batch_size,self.decoder_hidden_size)
+
+        # cell : 1 * batch_size * decode hidden size    1 * 128 * 64
+        cell = self.init_variable(1, batch_size, self.decoder_hidden_size)
+
+        # context : batch_size * decode_hidden   128 * 64
+        context = self.init_variable(batch_size, self.decoder_hidden_size)
 
         for t in range(self.T):
-            # (batch_size, T, (2 * decoder_hidden_size + encoder_hidden_size))
 
-            hidden_change = hidden.repeat(self.T, 1, 1).permute(1, 0, 2)
-            # print(hidden.size())
-            cell_change = cell.repeat(self.T, 1, 1).permute(1, 0, 2)
-            # print(cell.size())
-            # input_data_change = input_encoded.permute(0, 2, 1)
+            # Eqn. 12: concatenate the hidden states and the cell states
+            # 128 * 10 * 64 + 128 * 10 * 64 = 128 * 10 * 128
+            # batch_size * T * (2 * hidden_size)
+            x = torch.cat((self.embedding_hidden(hidden),self.embedding_hidden(cell)),dim=2)
 
-            x = torch.cat((hidden_change,cell_change,input_encoded), dim=2)
-            # Eqn. 12 & 13: softmax on the computed attention weights
-            x = F.softmax(
-                self.attn_layer(
-                    x.view(-1, 2 * self.decoder_hidden_size + self.encoder_hidden_size)
-                ).view(-1, self.T),
-                dim=1)  # (batch_size, T )
-            # print("x1",x)
-            # Eqn. 14: compute context vector
-            context = torch.bmm(x.unsqueeze(1), input_encoded).squeeze(1)  # (batch_size, encoder_hidden_size)
-            print('content',context.shape)
-            # print(y_history.size())
+            # batch_size * T * (2 * decode_hidden_size) -> batch_size * T * encode_hidden_size
+            # 128 * 10 * 128 -> (z1) 128 * 10 * 64
+            z1 = self.attn1(x)
+
+            # input_encoded: 128 * 10 * 64   batch_size * T * hidden_size
+            # output not change   128 * 10 * 64
+            z2 = self.attn2(input_encoded)
+
+            # z1 + z2   128 * 10 * 64 + 128 * 10 * 64 = 128 * 10 * 64
+            x = z1 + z2
+
+            # encode_hidden_size -> 1
+            # 128 * 10 * 64 -> 128 * 10 * 1
+            z3 = self.attn3(self.tanh(x))
+
+            # Eqn. 13: softmax to get weight
+            # x.view(): 128 * 10   weight 128 * 10
+            # beta_weight = F.softmax(x.view(batch_size, -1),dim=1)
+
+            if batch_size > 1:
+                beta_weight = F.softmax(z3.view(batch_size, -1), dim=1)
+            else:
+                beta_weight = self.init_variable(batch_size, self.decode_hidden_size) + 1
+
+            # Eqn. 14: sum to new context with tempial weight
+            # 128 * 1 * 10   bmm   128 * 10 * 64   = 128 * 1 * 64
+            # 128 * 1 * 64 -> 128 * 64
+            context = torch.bmm(beta_weight.unsqueeze(1),input_encoded).squeeze(1)
+
             # Eqn. 15
-            y_tilde = self.fc(torch.cat((context, y_history), dim=1))  # (batch_size, out_size)
-            print('y_title:',y_tilde.shape)
-            # Eqn. 16: LSTM
-            self.lstm_layer.flatten_parameters()
-            _, lstm_output = self.lstm_layer(y_tilde.unsqueeze(0), (hidden, cell))
-            hidden = lstm_output[0]  # 1 * batch_size * decoder_hidden_size
-            cell = lstm_output[1]  # 1 * batch_size * decoder_hidden_size
+            if t < self.T -1:
+
+                # yc: 128 * (1 + 64) -> 128 * 65
+                yc = torch.cat((y_seq[:, t].unsqueeze(1), context), dim=1)
+
+                # (batch_size, out_size)   128 * 1
+                y_tilde = self.tilde(yc)
+
+                # Eqn. 16: LSTM
+                # self.lstm_layer.flatten_parameters()
+
+                # y_tilde.unsqueeze:  1 * 128 * 1  hidden and cell : 1 * 128 * 64
+                _, lstm_state = self.lstm_layer(y_tilde.unsqueeze(0), (hidden, cell))
+
+                # 1 * batch_size * decoder_hidden_size
+                hidden = lstm_state[0]
+
+                # 1 * batch_size * decoder_hidden_size
+                cell = lstm_state[1]
 
         # Eqn. 22: final output
-        return self.fc_final(torch.cat((hidden[0], context), dim=1))
+        # hidden.squeeze : 128 *64(batch_size * decoder_hidden_size)
+        # context : 128 * 64   batch_size * decoder_hidden_size
+        # fc1 cat 128 * 128 -> 128 * 64
+        # fc2 128 * 64 -> 128 * 2
+
+        return self.fc2(self.fc1(torch.cat((hidden.squeeze(0), context), dim=1)))
         # return F.softmax(x,dim=1)
 
 
-    def init_hidden(self,x, hidden_size):
+    def init_variable(self, *args):
+        zero_tensor = torch.zeros(args)
         if torch.cuda.is_available():
-            return Variable(torch.zeros(1, x.size(0), hidden_size)).cuda()
-        else:
-            return Variable(torch.zeros(1, x.size(0), hidden_size))
+            zero_tensor = zero_tensor.cuda()
+        return Variable(zero_tensor)
+
+    def embedding_hidden(self, x):
+        return x.repeat(self.T, 1, 1).permute(1, 0, 2)

train.py
@@ -17,26 +17,29 @@ TARGET = "D:\Projects\\stock_predict\\stock_data\\Predicting-the-Dow-Jones-with-
 DECODER_HIDDEN_SIZE = 64
 ENCODER_HIDDEN_SIZE = 64
 TIME_STEP = 10
-SPLIT = 0.8
-LR = 0.03
+SPLIT = 0.6
+LR = 0.001
+momentum = 0.9
 num_epochs = 50
-batch_size = 64
+batch_size = 128
 interval = 5
 # best_dev_acc = 0.0
 
 class Trainer:
 
-    def __init__(self, driving, target, time_step, split, lr):
-        # self.dataset = DataSet(driving, target, time_step, split)
-        #
-        #
-        # f = open('dataset_obj.txt','wb')
-        # pickle.dump(self.dataset,f)
-        # f.close()
+    def __init__(self, driving, target, time_step, split,lr):
+        self.dataset = DataSet(driving, target, time_step, split)
 
-        f = open('dataset_obj.txt','rb')
-        self.dataset = pickle.load(f)
+
+        f = open('dataset_obj.txt','wb')
+        pickle.dump(self.dataset,f)
         f.close()
+
+        print('save model finish!!!!!!!!!!!!!!!!!!')
+
+        # f = open('dataset_obj.txt','rb')
+        # self.dataset = pickle.load(f)
+        # f.close()
 
         self.encoder = Encoder(input_size=self.dataset.get_num_features(), hidden_size=ENCODER_HIDDEN_SIZE,T=time_step)
         self.decoder = Decoder(encoder_hidden_size=ENCODER_HIDDEN_SIZE,  decoder_hidden_size=DECODER_HIDDEN_SIZE,T=time_step)
@@ -79,29 +82,36 @@ class Trainer:
                 var_y_seq = self.to_variable(y_seq_train[i: batch_end])
                 if var_x.dim() == 2:
                     var_x = var_x.unsqueeze(2)
-                weight1,code = self.encoder(var_x)
-                # print('code_size:',code.size())
+                code = self.encoder(var_x)
+
                 y_res = self.decoder(code, var_y_seq)
 
-                # print('see what the pred and truth')
-                # print('y_res:',y_res.shape,' : ',y_res)
-                # print('var_y:',var_y.shape,' : ',var_y)
-
-                pred = torch.max(F.softmax(y_res, dim=1), 1)[1]
-                # print(pred.shape,':',pred)
-                pred_y = pred.data.cpu().numpy().squeeze()
-                # print(pred_y.shape,':',pred_y)
-                # print()
-                pred_res_total.extend(pred_y)
-
                 loss = self.loss_func(y_res, var_y)
+                if i == 0:
+                    print("y_res:",y_res)
+                    print("var_y:",var_y)
                 loss.backward()
                 self.encoder_optim.step()
                 self.decoder_optim.step()
                 loss_sum += loss.item()
 
-                #update the i
+                # update the i
                 i = batch_end
+
+                pred_y = y_res.data.cpu()
+
+                # print('see what the pred and truth')
+                # print('y_res:',y_res.shape,' : ',y_res)
+                # print('var_y:',var_y.shape,' : ',var_y)
+
+                pred_y = torch.max(F.softmax(pred_y,dim=1), 1)[1]
+                #
+                # print('pred_y:',pred_y)
+                # print('var_y',var_y)
+
+                pred_res_total.extend(pred_y)
+
+
 
                 # if i%50 == 0:
                 #     print('         finish {0:.2f}/100'.format(i/self.train_size))
@@ -128,40 +138,7 @@ class Trainer:
 
         return train_acc_list, dev_acc_list,train_loss_list,dev_loss_list
 
-    # def test(self,batch_size,is_test=False):
-    #     x_test, y_test, y_seq_test = self.dataset.get_test_set()
-    #     i = 0
-    #     res = []
-    #     # y_labels = []
-    #     # for y in y_test:
-    #     #     if y[0] == 0:
-    #     #         y_labels.append(1)
-    #     #     else:
-    #     #         y_labels.append(0)
-    #
-    #     while i < self.test_size:
-    #
-    #         batch_end = i + batch_size
-    #         if batch_end >= self.test_size:
-    #             batch_end = self.test_size
-    #         var_x = self.to_variable(x_test[i: batch_end])
-    #         var_y = Variable(torch.from_numpy(y_test[i: batch_end]).long()).cuda()
-    #         # var_y = self.to_variable(y_test[i: batch_end])
-    #         var_y_seq = self.to_variable(y_seq_test[i: batch_end])
-    #         if var_x.dim() == 2:
-    #             var_x = var_x.unsqueeze(2)
-    #         weight1, code = self.encoder(var_x)
-    #         # print('code_size:',code.size())
-    #         y_res = self.decoder(code, var_y_seq)
-    #         # y_res = torch.sigmoid(y_res)
-    #         pred = torch.max(F.softmax(y_res,dim=1), 1)[1]
-    #         pred_y = pred.data.cpu().numpy().squeeze()
-    #         res.extend(pred_y)
-    #         i = batch_end
-    #
-    #     res = np.array(res)
-    #
-    #     return
+
 
     def test(self,batch_size,is_test=False):
         if not is_test:
@@ -183,15 +160,18 @@ class Trainer:
             var_y_seq = self.to_variable(y_seq[i: batch_end])
             if var_x.dim() == 2:
                 var_x = var_x.unsqueeze(2)
-            weight1, code = self.encoder(var_x)
 
+            # to encoder get encoder output
+            code = self.encoder(var_x)
+
+            # to decoder get classification
             y_res = self.decoder(code, var_y_seq)
 
             loss = self.loss_func(y_res, var_y)
             loss_sum += loss.item()
 
-            pred = torch.max(F.softmax(y_res,dim=1), 1)[1]
-            pred_y = pred.data.cpu().numpy().squeeze()
+            pred_y = y_res.data.cpu()
+            pred_y = torch.max(pred_y, 1)[1]
             res.extend(pred_y)
             i = batch_end
 